49d9633a042640051d321e9b0631756de8802e32
GabriPini <lele19977@gmail.com>
2022-03-23 13:38:30 +0100

    initial and final commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+//Chiediamo il nome 
+const First_name = prompt('Inserisci il tuo nome')
+console.log(First_name)
+
+
+//chiediamo il cognome
+const last_name = prompt('Inserisci il tuo cognome')
+console.log(last_name)
+
+
+//chidiamo il colore preferito 
+const Color_fav = prompt('Inserisci il tuo colore preferito')
+console.log(Color_fav)
+
+//ecco il risultato per la password 
+
+document.getElementById('Password').innerHTML = `${First_name}${last_name}${Color_fav}`
